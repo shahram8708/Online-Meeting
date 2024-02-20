1daebf3190abeacf44c09b9d9be33429e86e43d1
shah.ram <ram.coding8@gmail.com>
2024-02-20 19:18:55 +0530

    added commit

--- index.js
+++ index.js
@@ -1,63 +1,58 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static(__dirname + '/public'));
 
 const users = {};
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
+    console.log('New user connected');
 
-  socket.on('join', (userData) => {
-    const { userId, userName } = userData;
-    users[userId] = socket.id;
-    console.log(`User ${userName} (${userId}) joined the meeting`);
+    socket.on('join', ({ userId, userName }) => {
+        users[userId] = userName;
+        console.log(`User ${userId} joined the meeting`);
+    });
 
-    // Notify other users about the new participant
-    socket.broadcast.emit('userJoined', { userId, userName });
-  });
+    socket.on('joinRequest', ({ userId, otherUserId }) => {
+        if (users[otherUserId]) {
+            socket.emit('joinResponse', { success: true });
+            socket.to(otherUserId).emit('joinRequest', { userId });
+        } else {
+            socket.emit('joinResponse', { success: false });
+        }
+    });
 
-  socket.on('disconnect', () => {
-    const userId = getKeyByValue(users, socket.id);
-    if (userId) {
-      const userName = users[userId];
-      delete users[userId];
-      console.log(`User ${userName} (${userId}) left the meeting`);
+    socket.on('disconnect', () => {
+        const userId = getKeyByValue(users, socket.id);
+        if (userId) {
+            delete users[userId];
+            console.log(`User ${userId} left the meeting`);
+        }
+    });
 
-      // Notify other users about the participant who left
-      io.emit('userLeft', { userId, userName });
-    }
-  });
+    socket.on('chatMessage', ({ userId, userName, message }) => {
+        io.emit('chatMessage', { userId, userName, message });
+    });
 
-  socket.on('callUser', ({ userToCall, signalData }) => {
-    const socketId = users[userToCall];
-    io.to(socketId).emit('incomingCall', { signalData, from: socket.id });
-  });
+    socket.on('callUser', ({ userIdToCall, signalData }) => {
+        const socketId = users[userIdToCall];
+        io.to(socketId).emit('incomingCall', { signalData, from: socket.id });
+    });
 
-  socket.on('acceptCall', ({ callerId, signalData }) => {
-    io.to(users[callerId]).emit('callAccepted', { signalData, receiverId: socket.id });
-  });
-
-  socket.on('chatMessage', ({ userId, message }) => {
-    const userName = users[userId];
-    io.emit('chatMessage', { userId, userName, message });
-  });
+    socket.on('acceptCall', ({ receiverId, signalData }) => {
+        io.to(receiverId).emit('callAccepted', signalData);
+    });
 });
 
 function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
+    return Object.keys(object).find(key => object[key] === value);
 }
 
 server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
